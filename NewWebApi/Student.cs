4454bf28e539ca290e3977e5b4a1b3e5b2b9a6bd
vedant-mishra-ix <mishravedant040@gmail.com>
2022-08-08 16:44:23 +0530

    Core web api

---
+++ NewWebApi/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWebApi
+{
+    public class Student
+    {
+        public dynamic Id { get; set; }
+        public dynamic Message { get; set; }
+    }
+}
